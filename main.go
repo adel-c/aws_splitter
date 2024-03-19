860d7a881f8fa24ec9b0ee0a6e40aa01a6abbd91
Adel <576853-astran@users.noreply.gitlab.com>
2024-03-19 22:51:13 +0100

    wip

--- main.go
+++ main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "aws_splitter",
+	Short: "split cloudwatch log to separated files",
+	Long:  `split cloudwatch log to separated files`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		printLog = logNoop
+		if flags.verbose {
+			printLog = logOut
+		}
+		return runCommand()
+	},
+}
+
+var flags struct {
+	filepath string
+	verbose  bool
+}
+
+var flagsName = struct {
+	file, fileShort       string
+	verbose, verboseShort string
+}{
+	"file", "f",
+	"verbose", "v",
+}
+
+var printLog func(s string)
+
 func main() {
-	println("machi")
+	rootCmd.Flags().StringVarP(
+		&flags.filepath,
+		flagsName.file,
+		flagsName.fileShort,
+		"", "path to the file")
+	rootCmd.PersistentFlags().BoolVarP(
+		&flags.verbose,
+		flagsName.verbose,
+		flagsName.verboseShort,
+		false, "log verbose output")
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func logNoop(s string) {}
+
+func logOut(s string) {
+	log.Println(s)
 }
